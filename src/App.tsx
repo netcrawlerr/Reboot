f584f62748ac4a050ecaebc24e265c9709a0edca
netcrawlerr <abdulmejidamk@gmail.com>
2025-05-09 14:28:15 +0300

    "feat: route setup"

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
